c8bc27761ea5a8c1f6ed5ab5fc2d76a104ecc51a
Carolina Ferreira <carolina.ferreira@sydle.com>
2025-03-30 20:45:05 -0300

    pratica 1 parte 2

--- app/src/main/java/com/example/calculadora2/MainActivity.java
+++ app/src/main/java/com/example/calculadora2/MainActivity.java
@@ -13,8 +13,11 @@ import androidx.core.view.WindowInsetsCompat;
 
 import com.google.android.material.textfield.TextInputLayout;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.Arrays;
 
+public class MainActivity extends AppCompatActivity {
+   private int valor1 = -1, valor2 = -1;
+    private String op = "";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -26,69 +29,191 @@ public class MainActivity extends AppCompatActivity {
             return insets;
         });
 
+        Button bt_0 = findViewById(R.id.bt_0);
+        bt_0.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v){
+                TextInputLayout resultado = findViewById(R.id.textInput_A);
+                    op += '0';
+                    resultado.getEditText().setText(op);
+
+            }
+        });
+        Button bt_1 = findViewById(R.id.bt_1);
+        bt_1.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v){
+                TextInputLayout resultado = findViewById(R.id.textInput_A);
+                op += '1';
+                resultado.getEditText().setText(op);
+            }
+        });
+        Button bt_2 = findViewById(R.id.bt_2);
+        bt_2.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v){
+                TextInputLayout resultado = findViewById(R.id.textInput_A);
+                op += '2';
+                resultado.getEditText().setText(op);
+            }
+        });
+
+        Button bt_3 = findViewById(R.id.bt_3);
+        bt_3.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v){
+                TextInputLayout resultado = findViewById(R.id.textInput_A);
+                op += '3';
+                resultado.getEditText().setText(op);
+            }
+        });
+
+        Button bt_4 = findViewById(R.id.bt_4);
+        bt_4.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v){
+                TextInputLayout resultado = findViewById(R.id.textInput_A);
+                op += '4';
+                resultado.getEditText().setText(op);
+            }
+        });
+
+        Button bt_5 = findViewById(R.id.bt_5);
+        bt_5.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v){
+                TextInputLayout resultado = findViewById(R.id.textInput_A);
+                op += '5';
+                resultado.getEditText().setText(op);
+            }
+        });
+        Button bt_6 = findViewById(R.id.bt_6);
+        bt_6.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v){
+                TextInputLayout resultado = findViewById(R.id.textInput_A);
+                op += '6';
+                resultado.getEditText().setText(op);
+            }
+        });
+        Button bt_7 = findViewById(R.id.bt_7);
+        bt_7.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v){
+                TextInputLayout resultado = findViewById(R.id.textInput_A);
+                op += '7';
+                resultado.getEditText().setText(op);
+            }
+        });
+        Button bt_8 = findViewById(R.id.bt_8);
+        bt_8.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v){
+                TextInputLayout resultado = findViewById(R.id.textInput_A);
+                op += '8';
+                resultado.getEditText().setText(op);
+            }
+        });
+        Button bt_9 = findViewById(R.id.bt_9);
+        bt_9.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v){
+                TextInputLayout resultado = findViewById(R.id.textInput_A);
+                op += '9';
+                resultado.getEditText().setText(op);
+            }
+        });
         Button bt_soma = findViewById(R.id.Soma);
         bt_soma.setOnClickListener(new View.OnClickListener(){
             public void onClick(View v){
-                TextInputLayout valorA = findViewById(R.id.textInput_A);
-                TextInputLayout valorB = findViewById(R.id.textInput_B);
-
-                int vA =  Integer.parseInt(valorA.getEditText().getText().toString());
-                int vB =  Integer.parseInt(valorB.getEditText().getText().toString());
-
-                TextView resultado = findViewById(R.id.Resultado);
-
-                int res = vA + vB;
-                resultado.setText("O resultado é: " + String.valueOf(res));
+                TextInputLayout resultado = findViewById(R.id.textInput_A);
+                op += " + ";
+                resultado.getEditText().setText(op);
             }
         });
 
         Button bt_sub = findViewById(R.id.Sub);
         bt_sub.setOnClickListener(new View.OnClickListener(){
             public void onClick(View v){
-                TextInputLayout valorA = findViewById(R.id.textInput_A);
-                TextInputLayout valorB = findViewById(R.id.textInput_B);
-                TextView resultado = findViewById(R.id.Resultado);
-
-                int vA =  Integer.parseInt(valorA.getEditText().getText().toString());
-                int vB =  Integer.parseInt(valorB.getEditText().getText().toString());
-
-                int res = vA - vB;
-                resultado.setText("O resultado é: " + String.valueOf(res));
+                TextInputLayout resultado = findViewById(R.id.textInput_A);
+                op += " - ";
+                resultado.getEditText().setText(op);
             }
         });
 
         Button bt_mul = findViewById(R.id.Mul);
         bt_mul.setOnClickListener(new View.OnClickListener(){
             public void onClick(View v){
-                TextInputLayout valorA = findViewById(R.id.textInput_A);
-                TextInputLayout valorB = findViewById(R.id.textInput_B);
-                TextView resultado = findViewById(R.id.Resultado);
-
-                int vA =  Integer.parseInt(valorA.getEditText().getText().toString());
-                int vB =  Integer.parseInt(valorB.getEditText().getText().toString());
-
-                int res = vA * vB;
-                resultado.setText("O resultado é: " + String.valueOf(res));
+                TextInputLayout resultado = findViewById(R.id.textInput_A);
+                op += " x ";
+                resultado.getEditText().setText(op);
             }
         });
 
         Button bt_div = findViewById(R.id.Divi);
         bt_div.setOnClickListener(new View.OnClickListener(){
             public void onClick(View v){
-                TextInputLayout valorA = findViewById(R.id.textInput_A);
-                TextInputLayout valorB = findViewById(R.id.textInput_B);
-                TextView resultado = findViewById(R.id.Resultado);
+                TextInputLayout resultado = findViewById(R.id.textInput_A);
+                op += " / ";
+                resultado.getEditText().setText(op);
+            }
+        });
 
-                int vA =  Integer.parseInt(valorA.getEditText().getText().toString());
-                int vB =  Integer.parseInt(valorB.getEditText().getText().toString());
+        Button bt_ponto = findViewById(R.id.bt_ponto);
+        bt_ponto.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v){
+                TextInputLayout resultado = findViewById(R.id.textInput_A);
+                op += ".";
+                resultado.getEditText().setText(op);
+            }
+        });
+        Button bt_c = findViewById(R.id.bt_limpar);
+        bt_c.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v){
+                TextInputLayout resultado = findViewById(R.id.textInput_A);
+                op = "";
+                resultado.getEditText().setText(op);
+            }
+        });
+        Button bt_por = findViewById(R.id.bt_por);
+        bt_por.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v){
+                TextInputLayout resultado = findViewById(R.id.textInput_A);
+                op += " %";
+                resultado.getEditText().setText(op);
+            }
+        });
 
-                if(vB != 0) {
-                    int res = vA / vB;
-
-                    resultado.setText("O resultado é: " + String.valueOf(res));
-                }else {
-                    resultado.setText(" Não é possível fazer divisão por 0");
+        Button bt_igual = findViewById(R.id.bt_igual);
+        bt_igual.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v){
+                TextInputLayout resultado = findViewById(R.id.textInput_A);
+                double v1, v2, res = 0;
+                if(op.contains("x")){
+                    String[] parcelasMul = op.split(" ");
+                    v1 = Double.parseDouble(parcelasMul[0]);
+                    v2 = Double.parseDouble(parcelasMul[2]);
+                    res = v1*v2;
                 }
+                if(op.contains("/")){
+                    String[] parcelasMul = op.split(" ");
+                    v1 = Double.parseDouble(parcelasMul[0]);
+                    v2 = Double.parseDouble(parcelasMul[2]);
+                    if(v2 == 0) {
+                        resultado.getEditText().setText("ERROR");
+                        return;
+                    }
+                    res = v1/v2;
+                }
+                if(op.contains("+")){
+                    String[] parcelasMul = op.split(" ");
+                    v1 = Double.parseDouble(parcelasMul[0]);
+                    v2 = Double.parseDouble(parcelasMul[2]);
+                    res = v1+v2;
+                }
+                if(op.contains("-")){
+                    String[] parcelasMul = op.split(" ");
+                    v1 = Integer.parseInt(parcelasMul[0]);
+                    v2 = Integer.parseInt(parcelasMul[2]);
+                    res = v1-v2;
+                }
+                if(op.contains("%")){
+                    String[] parcelasMul = op.split(" ");
+                    v1 = Integer.parseInt(parcelasMul[0]);
+                    res = v1/100;
+                }
+
+                resultado.getEditText().setText(String.valueOf(res));
             }
         });
     }
